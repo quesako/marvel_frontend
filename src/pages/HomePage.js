436872ead580c4f9dc93ab21a60b8a6af518ce2d
pascal_b <quesako@me.com>
2022-11-20 15:27:15 +0100

    Boring Speach

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,17 +2,42 @@ import { useState } from 'react'
 import Loader from '../components/Loader'
 
 const HomePage = () => {
-    const [data, setData] = useState()
-    const [isLoading, setIsLoading] = useState(true)
-
-    console.log(data)
     return (
         <div>
-            {!isLoading ? (
-                <p>Super</p>
-            ) : (
-                <Loader message={'Loading...'} isLoading={isLoading} />
-            )}
+            <div
+                className={
+                    'mx-auto mt-48 max-w-4xl  px-4 text-center font-alt text-white'
+                }
+            >
+                <h1 className={'text-6xl font-bold'}> Welcome,</h1>
+
+                <p className={'text-xl'}>
+                    This website is not an official "Disney" website but a work
+                    project designed as part of a developer training...It's use
+                    an api based on express.
+                    <br /> With that, have a good day....
+                </p>
+                <div className={'mt-24 text-lg'}>
+                    <p>Build with:</p>
+                    <p className={'text-zinc-600'}>
+                        #react #tailwindcss #headless-ui #cookie-js
+                    </p>
+                </div>
+                <div className={'mt-24 text-lg'}>
+                    <p>@todo: </p>
+                    <p className={'text-zinc-600'}>1. Create notFound page</p>
+                    <p className={'text-zinc-600'}>
+                        2. Secure undefined data on single pages
+                    </p>
+                    <p className={'text-zinc-600'}>
+                        3. Refactoring to suppress duplicate code
+                    </p>
+                    <p className={'text-zinc-600'}>
+                        4. Make navigable comics list of character on character
+                        single page.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
